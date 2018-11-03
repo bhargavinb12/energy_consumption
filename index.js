f8b289c4aaaa8dfe1bacf2d19a8a8a5aa4d7db67
Narayan Bhat <bnarayan@uncc.edu>
2018-11-03 12:35:55 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+    function myFunction() {
+        var x = document.getElementById("demo1");
+        
+        x.innerHTML="<img src='image/car.PNG' alt='Default cars' height='420' width='420'>";
+        
+    }
+    function myFunction1() {
+        
+        var x = document.getElementById("demo1");
+        
+            x.innerHTML="<img src='image/blacktree.PNG' alt='Trees' height='420' width='420'>";
+
+      }
+    function myFunction2() {
+        
+        var x = document.getElementById("demo1");
+        
+            x.innerHTML="<img src='image/carbon1.PNG' alt='Co2' height='420' width='420'>";
+           
+        
+    }
